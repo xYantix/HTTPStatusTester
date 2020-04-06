61af125a73cabf15cee2484f00130ec7cb4cdb9b
xYantix <xyantix@net-sec.tech>
2020-04-06 18:14:09 +1000

    Added multithreading

--- statuscheck.py
+++ statuscheck.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import argparse
+import future
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-list', '-l', required=True, help='Enter path for domain/subdomain list')
@@ -12,7 +14,7 @@ args = parser.parse_args()
 urllist = args.list
 urls = open(urllist, 'r')
 
-for url in urls:
+def statuscheck(url):
     try:
         response = requests.get(url.strip(), timeout=1)
         status = response.status_code
@@ -21,3 +23,15 @@ for url in urls:
         outfile.write(url)
     except Exception:
         pass
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+    checks = {executor.submit(statuscheck, url): for url in urls}
+    for future in concurrent.futures.as_completed(checks):
+        url = checks[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            pass
+
+
